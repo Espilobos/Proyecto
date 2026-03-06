8f4c901399f9f99aa27bd62e6c3edd8f7fcf4258
Sebas <sebastian.espilobos@gmail.com>
2026-03-06 15:30:18 -0600

    Proyecto base

---
+++ src/main/java/com/siamese/demo/VeterinariaSiameseApplication.java
@@ -0,0 +1,13 @@
+package com.siamese.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaSiameseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaSiameseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siamese/demo/VeterinariaSiameseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siamese.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaSiameseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
