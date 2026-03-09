5823e162aa106d925485f49fa86aa42df2169e7b
alanafdz88 <alanafernandez0120@gmail.com>
2026-03-09 11:14:23 -0600

    commit 1: ya esta productos visible con datos del la BD local, se puede eliminar

---
+++ src/main/java/com/siamese/demo/service/ProductoService.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.siamese.demo.service;
+
+/**
+ *
+ * @author alana
+ */
+import com.siamese.demo.domain.Producto;
+import com.siamese.demo.repository.ProductoRepository;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ProductoService {
+  
+
+    private final ProductoRepository productoRepository;
+
+    public ProductoService(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo) {
+            return productoRepository.findByActivoTrue();
+        }
+        return productoRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoRepository.findById(idProducto);
+    }
+
+    @Transactional
+    public void save(Producto producto, MultipartFile imagenFile) {
+        producto = productoRepository.save(producto);
+
+        if (!imagenFile.isEmpty()) {
+            try {
+          
+                String carpeta = "src/main/resources/static/img/";
+                Path ruta = Paths.get(carpeta + imagenFile.getOriginalFilename());
+                Files.createDirectories(ruta.getParent());
+                Files.write(ruta, imagenFile.getBytes());
+
+                producto.setRutaImagen("/img/" + imagenFile.getOriginalFilename());
+                productoRepository.save(producto);
+            } catch (IOException e) {
+                throw new RuntimeException("Error al guardar la imagen.", e);
+            }
+        }
+    }
+
+    @Transactional
+    public void delete(Integer idProducto) {
+        if (!productoRepository.existsById(idProducto)) {
+            throw new IllegalArgumentException("El producto con ID " + idProducto + " no existe.");
+        }
+        try {
+            productoRepository.deleteById(idProducto);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("No se puede eliminar el producto. Tiene datos asociados.", e);
+        }
+    }
+}
+    
+
