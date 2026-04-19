4838aafc7cd2a850c3edca70107d56e92c470c3d
alanafdz88 <alanafernandez0120@gmail.com>
2026-04-19 13:04:39 -0600

    commit : todo lo de usuarios

---
+++ src/main/java/com/siamese/demo/repository/RolRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.siamese.demo.repository;
+
+
+import com.siamese.demo.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
+
+    
---
+++ src/main/java/com/siamese/demo/repository/RutaRepository.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.siamese.demo.repository;
+
+/**
+ *
+ * @author alana
+ */
+
+
+
+import com.siamese.demo.domain.Ruta;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RutaRepository extends JpaRepository<Ruta, Integer> {
+    
+    public List<Ruta> findAllByOrderByRequiereRolAsc();
+    
+}
